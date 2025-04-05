7ab725d83e3022cfd7a1cd83c46585d472e97783
Khushi Jaiswal <jaiswalkhushi2911@gmail.com>
2025-04-05 15:41:29 +0530

    Add files via upload

---
+++ Line Follower code/motor.h
@@ -0,0 +1,27 @@
+/*
+ * motor.h
+ *
+ * Created: 22-10-2024 18:59:20
+ *  Author: Khushi
+ */ 
+
+
+#ifndef MOTOR_H_
+#define MOTOR_H_
+
+#define F_CPU 8000000UL
+#include <avr/io.h>
+#include "motor.h"
+#include <util/delay.h>
+void forward(int leftspeed,int rightspeed);
+void backward(int leftspeed,int rightspeed);
+void left(int leftspeed,int rightspeed);
+void sharpleft(int leftspeed,int rightspeed);
+void right(int leftspeed,int rightspeed);
+void sharpright(int leftspeed,int rightspeed);
+void stop();
+
+
+
+
+#endif /* MOTOR_H_ */
